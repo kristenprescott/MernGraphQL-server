9c943e221c1ab362a6220e97f03c804e6640aac0
KayPea <prescottdarling@gmail.com>
2021-07-13 19:57:29 -0400

    fogot to init back here, so I did that. and connected MongoDB.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+// mongoose: ORM library, Object Relational Mapper, which allows interface w MongoDB
+const mongoose = require("mongoose");
+
+const { MONGODB_URI } = require("./config.js");
 
 // types for GraphQL:
 const typeDefs = gql`
@@ -23,9 +27,20 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// start server:
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`
+// chain our server to mongodb:
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`Connected to MongoDB - live is goood B^D`);
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`
     *----------------------------------*
     |----------------------------------|
     |-------- Server running: ---------|
@@ -34,7 +49,11 @@ server.listen({ port: 5000 }).then((res) => {
     |----------------------------------|
     |----------------------------------|
     `);
-});
+  })
+  .catch((err) => {
+    console.log(`DB Connection Error: ${err.message}`);
+    process.exit(-1);
+  });
 /*
 At localhost:5000 is the GraphQL playground, how cool is that?
 */
