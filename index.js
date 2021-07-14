96c6360459c5e444d169183ba51cff8d3bf6653c
KayPea <prescottdarling@gmail.com>
2021-07-13 21:37:40 -0400

    separated graphql queries & resolvers into their own directories

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
-const Post = require("./models/Post");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
 
 const port = process.env.PORT || 5000;
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
